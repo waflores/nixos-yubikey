8a7303d71ef613b830bcb5cf866f89f1cb6704ca
Will Flores <waflores956@gmail.com>
2026-05-29 22:07:54 -0400

    We moved the older version of this project into a notes subfolder.

---
+++ notes/runme.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xv
+
+# -usb -device usb-host,vendorid=1050,productid=0407
+qemu-system-x86_64 \
+    -enable-kvm \
+    -cpu max \
+    -cdrom ./result/iso/nixos-yubikey-20.03beta-156105.gfedcba-x86_64-linux.iso \
+    -device uefi-vars-x64,jsonfile=./vars.json \
+    -m 8G \
+    -smp 4 \
+    -usb -device u2f-passthru,hidraw=/dev/hidraw5
